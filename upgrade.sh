2618815aac22909e04fff9367702716e4d386360
GTrunSec <gtrunsec@hardenedlinux.org>
2021-01-03 13:04:59 -0800

    Sun Jan 03 13:05:04 PST 2021

--- upgrade.sh
+++ upgrade.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -p git -p yarn -p clojure -p nodejs -i bash
-set -euo pipefail
-
-cd logseq-src
-yarn && yarn upgrade && yarn release && yarn watch
+#!nix flake update --update-input logseq && nix-shell
